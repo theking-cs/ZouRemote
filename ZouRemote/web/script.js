8670123d63350fb51110596cdd32ed868e38e165
theking-cs <badryzoubair@gmail.com>
2026-05-07 22:52:34 +0200

    Delete ZouRemote directory

--- ZouRemote/web/script.js
+++
@@ -1,8 +0,0 @@
-const OPENWEBIF_IP = window.location.hostname;
-const OPENWEBIF_PORT = 80;
-
-function sendCommand(key) {
-    fetch(`http://${OPENWEBIF_IP}:${OPENWEBIF_PORT}/api/v1/remotecontrol?command=${key}`)
-        .then(response => console.log("Comando enviado:", key))
-        .catch(err => console.error("Error:", err));
-}
